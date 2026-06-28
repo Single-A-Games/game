815eb0cf0adaf6c0fafb637901b09e3f8af94009
oshawhat <alannobledall@gmail.com>
2026-06-28 00:53:51 -0500

    add wall jump

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ const SPEED = 5.0
 
 const JUMP_VELOCITY = 5.0
 const WALL_JUMP_VELOCITY = 4.0
+const WALL_JUMP_PUSH_VELOCITY = 8.0
 
 const MOVEMENT_ACCELERATION = 40.0
 const AIR_MOVEMENT_ACCELERATION = 25.0
@@ -16,6 +17,8 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var neck = $Neck
 @onready var camera = $Neck/Camera3D
+
+@onready var wall_jump_casts: Array[RayCast3D] = gen_wall_jump_raycasts()
 
 func _ready():
 	# Capture the mouse cursor inside the game window
@@ -45,6 +48,7 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		elif walls:
+			wall_jump()
 			velocity.y = WALL_JUMP_VELOCITY
 	
 	# Get WASD / Arrow keyboard inputs
@@ -57,3 +61,30 @@ func _physics_process(delta):
 	velocity = velocity.move_toward(Vector3(direction.x, velocity.y, direction.z), acceleration * delta)
 
 	move_and_slide()
+
+
+func wall_jump():
+	var vect_sum := Vector3.ZERO
+	for cast in wall_jump_casts:
+		if cast.is_colliding():
+			print("Raycast is colliding with target ", cast.target_position)
+			vect_sum += cast.target_position
+	var dir = vect_sum.normalized()
+	velocity -= dir * WALL_JUMP_PUSH_VELOCITY
+	print(dir)
+
+
+func gen_wall_jump_raycasts():
+	var num_casts := 16.0
+	var casts: Array[RayCast3D] = []
+	
+	for x in range(num_casts):
+		var new_cast = RayCast3D.new()
+		
+		new_cast.target_position = Vector3(.6, 0, 0).rotated(Vector3.UP, x / num_casts * 2 * PI)
+		new_cast.collision_mask = 0b0001
+		
+		add_child(new_cast)
+		casts.append(new_cast)
+	
+	return casts
