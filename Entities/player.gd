882c85fc746bb902d7a92d55f4f81c797c29ca9a
oshawhat <alannobledall@gmail.com>
2026-06-29 19:33:25 -0500

    fix sword and add slash effect

--- Entities/player.gd
+++ Entities/player.gd
@@ -4,6 +4,8 @@ class_name Player
 
 @onready var arm_tween: Tween
 
+var held_item = null
+
 
 func _physics_process(delta: float) -> void:
 	move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,26 +15,7 @@ func _physics_process(delta: float) -> void:
 
 
 func handle_actions(_delta: float) -> void:
-	var swing_degrees := 80.0
+	held_item = $Arm/Hand.get_child(0)
 	
-	if sin($Arm.rotation) < 0:
-		$Arm.z_index = -1
-	else:
-		$Arm.z_index = 1
-	
-	if Input.is_action_just_pressed("primary_attack"):
-		if arm_tween: return
-		
-		var ref_angle: float = rad_to_deg( global_position.angle_to_point(get_global_mouse_position()) )
-
-		$Arm.rotation_degrees = ref_angle - swing_degrees / 2
-		$Arm.show()
-		arm_tween = create_tween()
-		
-		arm_tween.tween_property($Arm, "rotation_degrees", ref_angle + swing_degrees / 2, .25).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-		
-		await arm_tween.finished
-		
-		arm_tween.kill()
-		arm_tween = null
-		$Arm.hide()
+	if Input.is_action_just_pressed("primary_attack") and held_item.is_ready():
+		held_item.use($Arm, $ItemEffect)
--- sword.gd
+++ sword.gd
@@ -2,14 +2,59 @@ extends Node2D
 
 
 @export var knockback = 300
+@export var cooldown := .75
+@export var swing_degrees := 150.0
+@export var slash := preload("res://assets/art/sword slash (2).png")
+
+var arm_tween: Tween
+var cooldown_timer := 0.0
 
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	$Area2D.monitoring = is_visible_in_tree()
+	cooldown_timer = max(0, cooldown_timer - delta)
+
+
+func use(arm: Node2D, item_effect: Sprite2D) -> void:
+	cooldown_timer = cooldown
+	
+	arm.z_index = sign(sin(arm.rotation))
+	
+	if arm_tween: return
+	
+	var ref_angle: float = global_position.angle_to_point(get_global_mouse_position())
+	
+	var offset = deg_to_rad(swing_degrees) / 2 * sign(cos(ref_angle))
+	
+	arm.rotation = ref_angle - offset
+	
+	arm.show()
+	arm_tween = create_tween()
+	
+	arm_tween.tween_property(arm, "rotation", ref_angle + offset, .3).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN_OUT)
+	arm_tween.tween_interval(.1)
+	
+	await get_tree().create_timer(.1).timeout
+	
+	item_effect.texture = slash
+	item_effect.rotation = ref_angle
+	item_effect.modulate.a = .5
+	
+	await arm_tween.finished
+	
+	arm_tween.kill()
+	arm_tween = null
+	arm.hide()
+	item_effect.texture = null
+	item_effect.modulate.a = 1
+
+
+func is_ready() -> bool:
+	return cooldown_timer == 0
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
